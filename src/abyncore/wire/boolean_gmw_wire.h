62273032147157da88ed3c0a1915c2009f43cce2
Oleksandr Tkachenko <tkachenko@encrypto.cs.tu-darmstadt.de>
2019-07-30 13:55:58 +0200

    + MIT license

--- src/abyncore/wire/boolean_gmw_wire.h
+++ src/abyncore/wire/boolean_gmw_wire.h
@@ -1,3 +1,27 @@
+// MIT License
+//
+// Copyright (c) 2019 Oleksandr Tkachenko
+// Cryptography and Privacy Engineering Group (ENCRYPTO)
+// TU Darmstadt, Germany
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include "wire.h"
@@ -12,9 +36,9 @@ namespace ABYN::Wires {
 
 class GMWWire : public BooleanWire {
  public:
-  GMWWire(ENCRYPTO::BitVector &&values, std::weak_ptr<Backend> reg, bool is_constant = false);
+  GMWWire(ENCRYPTO::BitVector<> &&values, std::weak_ptr<Backend> reg, bool is_constant = false);
 
-  GMWWire(const ENCRYPTO::BitVector &values, std::weak_ptr<Backend> reg, bool is_constant = false);
+  GMWWire(const ENCRYPTO::BitVector<> &values, std::weak_ptr<Backend> reg, bool is_constant = false);
 
   GMWWire(bool value, std::weak_ptr<Backend> reg, bool is_constant = false);
 
@@ -28,12 +52,12 @@ class GMWWire : public BooleanWire {
 
   std::size_t GetBitLength() const final { return 1; }
 
-  const ENCRYPTO::BitVector &GetValuesOnWire() const { return values_; }
+  const ENCRYPTO::BitVector<> &GetValuesOnWire() const { return values_; }
 
-  ENCRYPTO::BitVector &GetMutableValuesOnWire() { return values_; }
+  ENCRYPTO::BitVector<> &GetMutableValuesOnWire() { return values_; }
 
  private:
-  ENCRYPTO::BitVector values_;
+  ENCRYPTO::BitVector<> values_;
 };
 
 using GMWWirePtr = std::shared_ptr<GMWWire>;
