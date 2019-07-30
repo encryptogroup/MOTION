3257c82d350f6aa8c76a085a73d9749497477739
Oleksandr Tkachenko <tkachenko@encrypto.cs.tu-darmstadt.de>
2019-07-30 13:46:21 +0200

    ~ move stuff to object file

--- src/abyncore/share/share_wrapper.h
+++ src/abyncore/share/share_wrapper.h
@@ -1,3 +1,27 @@
+// MIT License
+//
+// Copyright (c) 2019 Oleksandr Tkachenko
+// Cryptography and Privacy Engineering Group (ENCRYPTO)
+// TU Darmstadt, Germany
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include "gate/arithmetic_gmw_gate.h"
@@ -17,56 +41,21 @@ class ShareWrapper {
   void operator=(SharePtr share) { share_ = share; }
   void operator=(const ShareWrapper &sw) { share_ = sw.share_; }
 
-  ShareWrapper operator^(const ShareWrapper &other) {
-    if (share_->GetSharingType() == MPCProtocol::ArithmeticGMW) {
-      throw std::runtime_error(
-          "Boolean primitive operations are only supported for Boolean GMW shares");
-    }
-    assert(share_);
-    assert(*other);
-    auto this_b = std::dynamic_pointer_cast<GMWShare>(share_);
-    auto other_b = std::dynamic_pointer_cast<GMWShare>(*other);
-
-    auto xor_gate = std::make_shared<Gates::GMW::GMWXORGate>(this_b, other_b);
-    share_->GetRegister()->RegisterNextGate(xor_gate);
-    return ShareWrapper(xor_gate->GetOutputAsShare());
-  }
+  ShareWrapper operator^(const ShareWrapper &other);
 
   void operator^=(const ShareWrapper &other) { *this = *this ^ other; }
 
-  std::shared_ptr<Share> operator&(const ShareWrapper &other) {
-    throw std::runtime_error(
-        "Arithmetic primitive operations are only supported for arithmetic GMW shares");
+  std::shared_ptr<Share> operator&([[maybe_unused]] const ShareWrapper &other) {
+    throw std::runtime_error("AND operation in GMW is not yet implemented");
   }
 
   void operator&=(const ShareWrapper &other) { *this = *this & other; }
 
-  ShareWrapper operator+(const ShareWrapper &other) {
-    assert(*other);
-    assert(share_->GetSharingType() == MPCProtocol::ArithmeticGMW);
-    assert(share_->GetSharingType() == other->GetSharingType());
-    assert(share_->GetBitLength() == other->GetBitLength());
-    if (share_->GetSharingType() != MPCProtocol::ArithmeticGMW) {
-      throw std::runtime_error(
-          "Arithmetic primitive operations are only supported for arithmetic GMW shares");
-    }
-
-    if (share_->GetBitLength() == 8u) {
-      return Add<std::uint8_t>(share_, *other);
-    } else if (share_->GetBitLength() == 16u) {
-      return Add<std::uint16_t>(share_, *other);
-    } else if (share_->GetBitLength() == 32u) {
-      return Add<std::uint32_t>(share_, *other);
-    } else if (share_->GetBitLength() == 64u) {
-      return Add<std::uint64_t>(share_, *other);
-    } else {
-      throw std::bad_cast();
-    }
-  }
+  ShareWrapper operator+(const ShareWrapper &other);
 
   void operator+=(const ShareWrapper &other) { *this = *this + other; }
 
-  std::shared_ptr<Share> operator*(const ShareWrapper &other) {
+  std::shared_ptr<Share> operator*([[maybe_unused]] const ShareWrapper &other) {
     throw std::runtime_error("Arithmetic GMW multiplication is not implemented yet");
   }
 
@@ -78,44 +67,7 @@ class ShareWrapper {
 
   const SharePtr &operator->() const { return share_; }
 
-  const SharePtr Out(std::size_t output_owner) {
-    assert(share_);
-    auto backend = share_->GetBackend().lock();
-    assert(backend);
-    switch (share_->GetSharingType()) {
-      case MPCProtocol::ArithmeticGMW: {
-        switch (share_->GetBitLength()) {
-          case 8u: {
-            return backend->ArithmeticGMWOutput<std::uint8_t>(share_, output_owner);
-          }
-          case 16u: {
-            return backend->ArithmeticGMWOutput<std::uint16_t>(share_, output_owner);
-          }
-          case 32u: {
-            return backend->ArithmeticGMWOutput<std::uint32_t>(share_, output_owner);
-          }
-          case 64u: {
-            return backend->ArithmeticGMWOutput<std::uint64_t>(share_, output_owner);
-          }
-          default: {
-            throw(std::runtime_error(
-                fmt::format("Unknown arithmetic ring of {} bilength", share_->GetBitLength())));
-          }
-        }
-      }
-      case MPCProtocol::BooleanGMW: {
-        return backend->BooleanGMWOutput(share_, output_owner);
-      }
-      case MPCProtocol::BMR: {
-        throw(std::runtime_error("BMR output gate is not implemented yet"));
-        // TODO
-      }
-      default: {
-        throw(std::runtime_error(fmt::format("Unknown protocol with id {}",
-                                             static_cast<uint>(share_->GetSharingType()))));
-      }
-    }
-  }
+  const SharePtr Out(std::size_t output_owner);
 
  private:
   SharePtr share_;
