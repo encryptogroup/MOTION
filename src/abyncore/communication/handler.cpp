3c90a1826a542c5de641b970e60681048f1ae51a
Oleksandr Tkachenko <tkachenko@encrypto.cs.tu-darmstadt.de>
2019-07-30 13:53:42 +0200

    + bytes sent, received; MIT license

--- src/abyncore/communication/handler.cpp
+++ src/abyncore/communication/handler.cpp
@@ -1,3 +1,27 @@
+// MIT License
+//
+// Copyright (c) 2019 Oleksandr Tkachenko
+// Cryptography and Privacy Engineering Group (ENCRYPTO)
+// TU Darmstadt, Germany
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "handler.h"
 
 #include <algorithm>
@@ -59,6 +83,8 @@ Handler::~Handler() {
   if (receiver_thread_.joinable()) {
     receiver_thread_.join();
   }
+  logger_->LogInfo(
+      fmt::format("{}: {}B sent {}B received", handler_info_, bytes_sent_, bytes_received_));
 }
 
 void Handler::SendMessage(flatbuffers::FlatBufferBuilder &message) {
@@ -166,6 +192,7 @@ void Handler::ActAsSender() {
           throw(std::runtime_error(fmt::format("Error while writing to socket: {}", ec.message())));
         }
         tmp_queue.pop();
+        bytes_sent_ += message.size() + sizeof(uint32_t);
       }
     }
   }
@@ -199,6 +226,8 @@ void Handler::ActAsReceiver() {
           flatbuffers::Verifier verifier(message_buffer.data(), message_buffer.size());
           assert(message->Verify(verifier));
         }
+
+        bytes_received_ += size + sizeof(uint32_t);
 
         if (message->message_type() == MessageType_TerminationMessage) {
           ReceivedTerminationMessage();
@@ -376,7 +405,7 @@ void Handler::Sync() {
     queue_send_.push(std::move(buffer));
   }
 
-  there_is_smth_to_send_->NotifyAll();
+  there_is_smth_to_send_->NotifyOne();
 
   auto shared_ptr_context = context_.lock();
   assert(shared_ptr_context);
--- src/abyncore/communication/handler.h
+++ src/abyncore/communication/handler.h
@@ -1,3 +1,27 @@
+// MIT License
+//
+// Copyright (c) 2019 Oleksandr Tkachenko
+// Cryptography and Privacy Engineering Group (ENCRYPTO)
+// TU Darmstadt, Germany
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include <queue>
@@ -71,6 +95,8 @@ class Handler {
   bool continue_communication_ = true;
   bool received_termination_message_ = false, sent_termination_message_ = false;
   std::unique_ptr<ENCRYPTO::Condition> received_new_msg_, there_is_smth_to_send_;
+  std::size_t bytes_sent_ = 0;
+  std::size_t bytes_received_ = 0;
 
   void ReceivedTerminationMessage() { received_termination_message_ = true; }
 
