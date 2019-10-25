86991b6104f801376fab612359f8cbf0086031ea
Oleksandr Tkachenko <tkachenko@encrypto.cs.tu-darmstadt.de>
2019-10-25 16:57:45 +0200

    + tests for AGMW SUB, BGMW EQ and OR

--- src/test/test_motion.cpp
+++ src/test/test_motion.cpp
@@ -457,6 +457,105 @@ TEST(ArithmeticGMW, Addition_1_1K_SIMD_2_3_4_5_10_parties) {
   }
 }
 
+TEST(ArithmeticGMW, Subtraction_1_1K_SIMD_2_3_4_5_10_parties) {
+  constexpr auto AGMW = MOTION::MPCProtocol::ArithmeticGMW;
+  std::srand(std::time(nullptr));
+  auto template_test = [](auto template_var) {
+    using T = decltype(template_var);
+    const std::vector<T> _zero_v_1K(1000, 0);
+    for (auto num_parties : num_parties_list) {
+      std::size_t output_owner = std::rand() % num_parties;
+      std::vector<T> in_1 = RandomVector<T>(num_parties);
+      std::vector<std::vector<T>> in_1K(num_parties);
+      for (auto &v : in_1K) {
+        v = RandomVector<T>(1000);
+      }
+      try {
+        std::vector<PartyPtr> motion_parties(std::move(GetNLocalParties(num_parties, PORT_OFFSET)));
+        for (auto &p : motion_parties) {
+          p->GetLogger()->SetEnabled(DETAILED_LOGGING_ENABLED);
+          p->GetConfiguration()->SetOnlineAfterSetup(std::random_device{}() % 2 == 1);
+        }
+#pragma omp parallel num_threads(motion_parties.size() + 1) default(shared)
+#pragma omp single
+#pragma omp taskloop num_tasks(motion_parties.size())
+        for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+          std::vector<MOTION::Shares::ShareWrapper> s_in_1, s_in_1K;
+          for (auto j = 0u; j < num_parties; ++j) {
+            // If my input - real input, otherwise a dummy 0 (-vector).
+            // Should not make any difference, just for consistency...
+            const T my_in_1 = party_id == j ? in_1.at(j) : 0;
+            const std::vector<T> &my_in_1K = party_id == j ? in_1K.at(j) : _zero_v_1K;
+
+            s_in_1.push_back(motion_parties.at(party_id)->IN<AGMW>(my_in_1, j));
+            s_in_1K.push_back(motion_parties.at(party_id)->IN<AGMW>(my_in_1K, j));
+          }
+
+          auto s_sub_1 = s_in_1.at(0) - s_in_1.at(1);
+          auto s_sub_1K = s_in_1K.at(0) - s_in_1K.at(1);
+
+          for (auto j = 2u; j < num_parties; ++j) {
+            s_sub_1 -= s_in_1.at(j);
+            s_sub_1K -= s_in_1K.at(j);
+          }
+
+          auto s_out_1 = s_sub_1.Out(output_owner);
+          auto s_out_1K = s_sub_1K.Out(output_owner);
+
+          auto s_out_1_all = s_sub_1.Out();
+          auto s_out_1K_all = s_sub_1K.Out();
+
+          motion_parties.at(party_id)->Run(2);
+
+          if (party_id == output_owner) {
+            auto wire_1 = std::dynamic_pointer_cast<MOTION::Wires::ArithmeticWire<T>>(
+                s_out_1->GetWires().at(0));
+            auto wire_1K = std::dynamic_pointer_cast<MOTION::Wires::ArithmeticWire<T>>(
+                s_out_1K->GetWires().at(0));
+
+            T circuit_result_1 = wire_1->GetValues().at(0);
+            T expected_result_1 = Helpers::SubReduction(in_1);
+            EXPECT_EQ(circuit_result_1, expected_result_1);
+
+            const std::vector<T> &circuit_result_1K = wire_1K->GetValues();
+            const std::vector<T> expected_result_1K = std::move(Helpers::RowSubReduction(in_1K));
+            for (auto i = 0u; i < circuit_result_1K.size(); ++i) {
+              EXPECT_EQ(circuit_result_1K.at(i), expected_result_1K.at(i));
+            }
+          }
+
+          {
+            auto wire_1 = std::dynamic_pointer_cast<MOTION::Wires::ArithmeticWire<T>>(
+                s_out_1_all->GetWires().at(0));
+            auto wire_1K = std::dynamic_pointer_cast<MOTION::Wires::ArithmeticWire<T>>(
+                s_out_1K_all->GetWires().at(0));
+
+            T circuit_result_1 = wire_1->GetValues().at(0);
+            T expected_result_1 = Helpers::SubReduction(in_1);
+            EXPECT_EQ(circuit_result_1, expected_result_1);
+
+            const std::vector<T> &circuit_result_1K = wire_1K->GetValues();
+            const std::vector<T> expected_result_1K = Helpers::RowSubReduction(in_1K);
+            for (auto i = 0u; i < circuit_result_1K.size(); ++i) {
+              EXPECT_EQ(circuit_result_1K.at(i), expected_result_1K.at(i));
+            }
+          }
+          motion_parties.at(party_id)->Finish();
+        }
+      } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+      }
+    }
+  };
+  for (auto i = 0ull; i < TEST_ITERATIONS; ++i) {
+    // lambdas don't support templates, but only auto types. So, let's try to trick them.
+    template_test(static_cast<std::uint8_t>(0));
+    template_test(static_cast<std::uint16_t>(0));
+    template_test(static_cast<std::uint32_t>(0));
+    template_test(static_cast<std::uint64_t>(0));
+  }
+}
+
 TEST(ArithmeticGMW, Multiplication_1_100_SIMD_2_3_parties) {
   constexpr auto AGMW = MOTION::MPCProtocol::ArithmeticGMW;
   std::srand(std::time(nullptr));
@@ -848,6 +947,153 @@ TEST(BooleanGMW, MUX_1K_SIMD_64_wires_2_3_parties) {
   }
 }
 
+TEST(BooleanGMW, EQ_1_bit_1K_SIMD_2_3_parties) {
+  constexpr auto GMW = MOTION::MPCProtocol::BooleanGMW;
+  std::srand(0);
+  constexpr std::size_t n_simd{1000}, n_wires{1};
+  for (auto num_parties : {2u, 3u}) {
+    std::size_t input_owner0 = std::rand() % num_parties, input_owner1 = input_owner0;
+    while (input_owner0 == input_owner1) input_owner1 = std::rand() % num_parties;
+    const std::size_t output_owner{std::rand() % num_parties};
+    std::vector<std::vector<ENCRYPTO::BitVector<>>> global_input(num_parties);
+    for (auto &bv_v : global_input) {
+      bv_v.resize(n_wires);
+      for (auto &bv : bv_v) {
+        bv = ENCRYPTO::BitVector<>::Random(n_simd);
+      }
+    }
+    std::vector<ENCRYPTO::BitVector<>> dummy_input(n_wires, ENCRYPTO::BitVector<>(n_simd, false));
+
+    std::vector<PartyPtr> motion_parties(GetNLocalParties(num_parties, PORT_OFFSET));
+    for (auto &p : motion_parties) {
+      p->GetLogger()->SetEnabled(DETAILED_LOGGING_ENABLED);
+      p->GetConfiguration()->SetOnlineAfterSetup(true);
+    }
+    std::vector<std::thread> t;
+    for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+      t.emplace_back([n_simd, n_wires, party_id, input_owner0, input_owner1, &motion_parties, this,
+                      output_owner, &global_input, &dummy_input]() {
+        std::vector<MOTION::Shares::ShareWrapper> s_in;
+
+        for (const auto input_owner : {input_owner0, input_owner1}) {
+          if (input_owner == motion_parties.at(party_id)->GetConfiguration()->GetMyId()) {
+            s_in.push_back(
+                motion_parties.at(party_id)->IN<GMW>(global_input.at(input_owner), input_owner));
+          } else {
+            s_in.push_back(motion_parties.at(party_id)->IN<GMW>(dummy_input, input_owner));
+          }
+        }
+
+        auto s_eq = (s_in.at(0) == s_in.at(1));
+
+        auto s_out = s_eq.Out(output_owner);
+
+        motion_parties.at(party_id)->Run();
+
+        if (party_id == output_owner) {
+          for (auto j = 0ull; j < s_out->GetWires().size(); ++j) {
+            auto wire_single =
+                std::dynamic_pointer_cast<MOTION::Wires::GMWWire>(s_out->GetWires().at(j));
+            assert(wire_single);
+
+            std::vector<ENCRYPTO::BitVector<>> eq_check_v(n_wires);
+            for (auto wire_i = 0ull; wire_i < n_wires; ++wire_i) {
+              for (auto simd_i = 0ull; simd_i < n_simd; ++simd_i) {
+                eq_check_v.at(wire_i).Append(global_input.at(input_owner0).at(wire_i)[simd_i] ==
+                                             global_input.at(input_owner1).at(wire_i)[simd_i]);
+              }
+            }
+
+            auto eq_check = eq_check_v.at(0);
+            for (auto wire_i = 1ull; wire_i < n_wires; ++wire_i) eq_check &= eq_check_v.at(wire_i);
+
+            EXPECT_EQ(wire_single->GetValues(), eq_check);
+          }
+        }
+        motion_parties.at(party_id)->Finish();
+      });
+    }
+    for (auto &tt : t)
+      if (tt.joinable()) tt.join();
+  }
+}
+
+TEST(BooleanGMW, EQ_64_bit_10_SIMD_2_3_parties) {
+  constexpr auto GMW = MOTION::MPCProtocol::BooleanGMW;
+  std::srand(0);
+  constexpr std::size_t n_simd{10}, n_wires{64};
+  for (auto num_parties : {2u, 3u}) {
+    std::size_t input_owner0 = std::rand() % num_parties, input_owner1 = input_owner0;
+    while (input_owner0 == input_owner1) input_owner1 = std::rand() % num_parties;
+    const std::size_t output_owner{std::rand() % num_parties};
+    std::vector<std::vector<ENCRYPTO::BitVector<>>> global_input(num_parties);
+    for (auto &bv_v : global_input) {
+      bv_v.resize(n_wires);
+      for (auto &bv : bv_v) {
+        bv = ENCRYPTO::BitVector<>::Random(n_simd);
+      }
+    }
+
+    // to guarantee that at least one EQ result is true
+    global_input.at(0).at(0).Set(true);
+    global_input.at(1).at(0).Set(true);
+
+    std::vector<ENCRYPTO::BitVector<>> dummy_input(n_wires, ENCRYPTO::BitVector<>(n_simd, false));
+
+    std::vector<PartyPtr> motion_parties(GetNLocalParties(num_parties, PORT_OFFSET));
+    for (auto &p : motion_parties) {
+      p->GetLogger()->SetEnabled(DETAILED_LOGGING_ENABLED);
+      p->GetConfiguration()->SetOnlineAfterSetup(true);
+    }
+    std::vector<std::thread> t;
+    for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+      t.emplace_back([party_id, input_owner0, input_owner1, &motion_parties, this, output_owner,
+                      &global_input, &dummy_input]() {
+        std::vector<MOTION::Shares::ShareWrapper> s_in;
+
+        for (const auto input_owner : {input_owner0, input_owner1}) {
+          if (input_owner == motion_parties.at(party_id)->GetConfiguration()->GetMyId()) {
+            s_in.push_back(
+                motion_parties.at(party_id)->IN<GMW>(global_input.at(input_owner), input_owner));
+          } else {
+            s_in.push_back(motion_parties.at(party_id)->IN<GMW>(dummy_input, input_owner));
+          }
+        }
+
+        auto s_eq = (s_in.at(0) == s_in.at(1));
+
+        auto s_out = s_eq.Out(output_owner);
+
+        motion_parties.at(party_id)->Run();
+
+        if (party_id == output_owner) {
+          for (auto j = 0ull; j < s_out->GetWires().size(); ++j) {
+            auto wire_single =
+                std::dynamic_pointer_cast<MOTION::Wires::GMWWire>(s_out->GetWires().at(j));
+            assert(wire_single);
+
+            std::vector<ENCRYPTO::BitVector<>> eq_check_v(n_wires);
+            for (auto wire_i = 0ull; wire_i < n_wires; ++wire_i) {
+              for (auto simd_i = 0ull; simd_i < n_simd; ++simd_i) {
+                eq_check_v.at(wire_i).Append(global_input.at(input_owner0).at(wire_i)[simd_i] ==
+                                             global_input.at(input_owner1).at(wire_i)[simd_i]);
+              }
+            }
+
+            auto eq_check = eq_check_v.at(0);
+            for (auto wire_i = 1ull; wire_i < n_wires; ++wire_i) eq_check &= eq_check_v.at(wire_i);
+
+            EXPECT_EQ(wire_single->GetValues(), eq_check);
+          }
+        }
+        motion_parties.at(party_id)->Finish();
+      });
+    }
+    for (auto &tt : t)
+      if (tt.joinable()) tt.join();
+  }
+}
+
 TEST(BooleanGMW, AND_1_bit_1_1K_SIMD_2_3_parties) {
   for (auto i = 0ull; i < TEST_ITERATIONS; ++i) {
     constexpr auto BGMW = MOTION::MPCProtocol::BooleanGMW;
@@ -1015,6 +1261,154 @@ TEST(BooleanGMW, AND_64_bit_10_SIMD_2_3_parties) {
 
               EXPECT_EQ(wire_single->GetValues(),
                         ENCRYPTO::BitVector<>::ANDBitVectors(global_input_single));
+            }
+          }
+
+          motion_parties.at(party_id)->Finish();
+        }
+      } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+      }
+    }
+  }
+}
+
+TEST(BooleanGMW, OR_1_bit_1_1K_SIMD_2_3_parties) {
+  for (auto i = 0ull; i < TEST_ITERATIONS; ++i) {
+    constexpr auto BGMW = MOTION::MPCProtocol::BooleanGMW;
+    std::srand(std::time(nullptr));
+    for (auto num_parties : {2u, 3u}) {
+      const std::size_t output_owner = std::rand() % num_parties;
+      std::vector<bool> global_input_1(num_parties);
+      for (auto j = 0ull; j < global_input_1.size(); ++j) {
+        global_input_1.at(j) = (std::rand() % 2) == 1;
+      }
+      std::vector<ENCRYPTO::BitVector<>> global_input_1K(num_parties);
+
+      for (auto j = 0ull; j < global_input_1K.size(); ++j) {
+        global_input_1K.at(j) = ENCRYPTO::BitVector<>::Random(1000);
+      }
+      bool dummy_input_1 = false;
+      ENCRYPTO::BitVector<> dummy_input_1K(1000, false);
+      try {
+        std::vector<PartyPtr> motion_parties(std::move(GetNLocalParties(num_parties, PORT_OFFSET)));
+        for (auto &p : motion_parties) {
+          p->GetLogger()->SetEnabled(DETAILED_LOGGING_ENABLED);
+          p->GetConfiguration()->SetOnlineAfterSetup(i % 2 == 1);
+        }
+
+        auto f = [&](std::size_t party_id) {
+          std::vector<MOTION::Shares::ShareWrapper> s_in_1, s_in_1K;
+
+          for (auto j = 0ull; j < num_parties; ++j) {
+            if (j == motion_parties.at(party_id)->GetConfiguration()->GetMyId()) {
+              s_in_1.push_back(motion_parties.at(party_id)->IN<BGMW>(
+                  static_cast<bool>(global_input_1.at(j)), j));
+              s_in_1K.push_back(motion_parties.at(party_id)->IN<BGMW>(global_input_1K.at(j), j));
+            } else {
+              s_in_1.push_back(motion_parties.at(party_id)->IN<BGMW>(dummy_input_1, j));
+              s_in_1K.push_back(motion_parties.at(party_id)->IN<BGMW>(dummy_input_1K, j));
+            }
+          }
+
+          auto s_or_1 = s_in_1.at(0) | s_in_1.at(1);
+          auto s_or_1K = s_in_1K.at(0) | s_in_1K.at(1);
+
+          for (auto j = 2ull; j < num_parties; ++j) {
+            s_or_1 = s_or_1 | s_in_1.at(j);
+            s_or_1K = s_or_1K | s_in_1K.at(j);
+          }
+
+          auto s_out_1_all = s_or_1.Out();
+          auto s_out_1K_all = s_or_1K.Out();
+
+          motion_parties.at(party_id)->Run();
+
+          {
+            auto wire_1 =
+                std::dynamic_pointer_cast<MOTION::Wires::GMWWire>(s_out_1_all->GetWires().at(0));
+            auto wire_1K =
+                std::dynamic_pointer_cast<MOTION::Wires::GMWWire>(s_out_1K_all->GetWires().at(0));
+
+            assert(wire_1);
+            assert(wire_1K);
+
+            EXPECT_EQ(wire_1->GetValues().Get(0),
+                      ENCRYPTO::BitVector<>::ORReduceBitVector(global_input_1));
+            EXPECT_EQ(wire_1K->GetValues(), ENCRYPTO::BitVector<>::ORBitVectors(global_input_1K));
+          }
+          motion_parties.at(party_id)->Finish();
+        };
+
+#pragma omp parallel for num_threads(motion_parties.size() + 1)
+        for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+          f(party_id);
+        }
+      } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+      }
+    }
+  }
+}
+
+TEST(BooleanGMW, OR_64_bit_10_SIMD_2_3_parties) {
+  for (auto i = 0ull; i < TEST_ITERATIONS; ++i) {
+    constexpr auto BGMW = MOTION::MPCProtocol::BooleanGMW;
+    std::srand(std::time(nullptr));
+    for (auto num_parties : {2u, 3u}) {
+      const std::size_t output_owner = std::rand() % num_parties;
+      std::vector<std::vector<ENCRYPTO::BitVector<>>> global_input_10_64_bit(num_parties);
+      for (auto &bv_v : global_input_10_64_bit) {
+        bv_v.resize(64);
+        for (auto &bv : bv_v) {
+          bv = ENCRYPTO::BitVector<>::Random(10);
+        }
+      }
+      std::vector<ENCRYPTO::BitVector<>> dummy_input_10_64_bit(64,
+                                                               ENCRYPTO::BitVector<>(10, false));
+
+      try {
+        std::vector<PartyPtr> motion_parties(std::move(GetNLocalParties(num_parties, PORT_OFFSET)));
+        for (auto &p : motion_parties) {
+          p->GetLogger()->SetEnabled(DETAILED_LOGGING_ENABLED);
+          p->GetConfiguration()->SetOnlineAfterSetup(i % 2 == 1);
+        }
+#pragma omp parallel for num_threads(motion_parties.size() + 1)
+        for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+          std::vector<MOTION::Shares::ShareWrapper> s_in;
+
+          for (auto j = 0ull; j < num_parties; ++j) {
+            if (j == motion_parties.at(party_id)->GetConfiguration()->GetMyId()) {
+              s_in.push_back(
+                  motion_parties.at(party_id)->IN<BGMW>(global_input_10_64_bit.at(j), j));
+            } else {
+              s_in.push_back(motion_parties.at(party_id)->IN<BGMW>(dummy_input_10_64_bit, j));
+            }
+          }
+
+          auto s_or = s_in.at(0) | s_in.at(1);
+
+          for (auto j = 2ull; j < num_parties; ++j) {
+            s_or = s_or | s_in.at(j);
+          }
+
+          auto s_out = s_or.Out(output_owner);
+
+          motion_parties.at(party_id)->Run();
+
+          if (party_id == output_owner) {
+            for (auto j = 0ull; j < global_input_10_64_bit.size(); ++j) {
+              auto wire_single =
+                  std::dynamic_pointer_cast<MOTION::Wires::GMWWire>(s_out->GetWires().at(j));
+              assert(wire_single);
+
+              std::vector<ENCRYPTO::BitVector<>> global_input_single;
+              for (auto k = 0ull; k < num_parties; ++k) {
+                global_input_single.push_back(global_input_10_64_bit.at(k).at(j));
+              }
+
+              EXPECT_EQ(wire_single->GetValues(),
+                        ENCRYPTO::BitVector<>::ORBitVectors(global_input_single));
             }
           }
 
