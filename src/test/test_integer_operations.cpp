956b89131ca0705e08f5e94b05696a8a9cc729a9
Lennart Braun <lenerd@posteo.de>
2019-11-15 17:40:29 +0100

    Call lambda functions in test case
    Someone forgot to add the `()`.

--- src/test/test_integer_operations.cpp
+++ src/test/test_integer_operations.cpp
@@ -45,33 +45,33 @@ TEST(AlgorithmDescription, FromBristolFormat__int_add8_size) {
   EXPECT_EQ(int_add8.gates_.size(), 34);
   EXPECT_EQ(int_add8.n_output_wires_, 8);
   EXPECT_EQ(int_add8.n_input_wires_parent_a_, 8);
-  ASSERT_NO_THROW([&int_add8]() { EXPECT_EQ(*int_add8.n_input_wires_parent_b_, 8); });
+  ASSERT_NO_THROW([&int_add8]() { EXPECT_EQ(*int_add8.n_input_wires_parent_b_, 8); }());
   EXPECT_EQ(int_add8.n_wires_, 50);
 
   const auto &gate0 = int_add8.gates_.at(0);
   EXPECT_EQ(gate0.parent_a_, 0);
-  ASSERT_NO_THROW([&gate0]() { EXPECT_EQ(*gate0.parent_b_, 8); });
+  ASSERT_NO_THROW([&gate0]() { EXPECT_EQ(*gate0.parent_b_, 8); }());
   EXPECT_EQ(gate0.output_wire_, 42);
   EXPECT_EQ(gate0.type_, ENCRYPTO::PrimitiveOperationType::XOR);
   EXPECT_EQ(gate0.selection_bit_.has_value(), false);
 
   const auto &gate1 = int_add8.gates_.at(1);
   EXPECT_EQ(gate1.parent_a_, 0);
-  ASSERT_NO_THROW([&gate1]() { EXPECT_EQ(*gate1.parent_b_, 8); });
+  ASSERT_NO_THROW([&gate1]() { EXPECT_EQ(*gate1.parent_b_, 8); }());
   EXPECT_EQ(gate1.output_wire_, 16);
   EXPECT_EQ(gate1.type_, ENCRYPTO::PrimitiveOperationType::AND);
   EXPECT_EQ(gate1.selection_bit_.has_value(), false);
 
   const auto &gate32 = int_add8.gates_.at(32);
   EXPECT_EQ(gate32.parent_a_, 15);
-  ASSERT_NO_THROW([&gate32]() { EXPECT_EQ(*gate32.parent_b_, 40); });
+  ASSERT_NO_THROW([&gate32]() { EXPECT_EQ(*gate32.parent_b_, 40); }());
   EXPECT_EQ(gate32.output_wire_, 41);
   EXPECT_EQ(gate32.type_, ENCRYPTO::PrimitiveOperationType::XOR);
   EXPECT_EQ(gate32.selection_bit_.has_value(), false);
 
   const auto &gate33 = int_add8.gates_.at(33);
   EXPECT_EQ(gate33.parent_a_, 7);
-  ASSERT_NO_THROW([&gate33]() { EXPECT_EQ(*gate33.parent_b_, 41); });
+  ASSERT_NO_THROW([&gate33]() { EXPECT_EQ(*gate33.parent_b_, 41); }());
   EXPECT_EQ(gate33.output_wire_, 49);
   EXPECT_EQ(gate33.type_, ENCRYPTO::PrimitiveOperationType::XOR);
   EXPECT_EQ(gate33.selection_bit_.has_value(), false);
