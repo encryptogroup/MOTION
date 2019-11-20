bb038b605b33589dafcd3785741b8ca6c436ad08
Lennart Braun <lenerd@posteo.de>
2019-11-20 19:48:03 +0100

    Add bytes/unaligned functions for AES CTR RNG

--- src/motioncore/crypto/random/aes128_ctr_rng.cpp
+++ src/motioncore/crypto/random/aes128_ctr_rng.cpp
@@ -24,7 +24,6 @@
 #include "aes128_ctr_rng.h"
 #include "crypto/aes/aesni_primitives.h"
 
-
 thread_local AES128_CTR_RNG AES128_CTR_RNG::thread_instance_;
 
 struct AES128_CTR_RNG::AES128_CTR_RNG_State {
@@ -32,7 +31,9 @@ struct AES128_CTR_RNG::AES128_CTR_RNG_State {
   std::uint64_t counter;
 };
 
-AES128_CTR_RNG::AES128_CTR_RNG() : state_(std::make_unique<AES128_CTR_RNG_State>()) { sample_key(); }
+AES128_CTR_RNG::AES128_CTR_RNG() : state_(std::make_unique<AES128_CTR_RNG_State>()) {
+  sample_key();
+}
 
 AES128_CTR_RNG::~AES128_CTR_RNG() = default;
 
@@ -58,13 +59,22 @@ void AES128_CTR_RNG::sample_key() {
 
 void AES128_CTR_RNG::random_blocks_aligned(std::byte* output, std::size_t num_blocks) {
   std::byte* aligned_output = reinterpret_cast<std::byte*>(__builtin_assume_aligned(output, 16));
-  aesni_ctr_stream_blocks_128(state_->round_keys.data(), &state_->counter, aligned_output, num_blocks);
+  aesni_ctr_stream_blocks_128(state_->round_keys.data(), &state_->counter, aligned_output,
+                              num_blocks);
 }
 
-void AES128_CTR_RNG::random_blocks(std::byte*, std::size_t) {
-  throw std::logic_error("AES128_CTR_RNG::random_blocks is not implemented yet");
+void AES128_CTR_RNG::random_blocks(std::byte* output, std::size_t num_blocks) {
+  aesni_ctr_stream_blocks_128_unaligned(state_->round_keys.data(), &state_->counter, output,
+                                        num_blocks);
 }
 
-void AES128_CTR_RNG::random_bytes(std::byte*, std::size_t) {
-  throw std::logic_error("AES128_CTR_RNG::random_bytes is not implemented yet");
+void AES128_CTR_RNG::random_bytes(std::byte* output, std::size_t num_bytes) {
+  std::size_t num_blocks = num_bytes / aes_block_size;
+  std::size_t remaining_bytes = num_bytes & aes_block_size;
+  random_blocks(output, num_blocks);
+  std::array<std::byte, aes_block_size> extra_block;
+  aesni_ctr_stream_single_block_128_unaligned(state_->round_keys.data(), &state_->counter,
+                                              extra_block.data());
+  std::copy(std::begin(extra_block), std::begin(extra_block) + remaining_bytes,
+            output + num_bytes - remaining_bytes);
 }
