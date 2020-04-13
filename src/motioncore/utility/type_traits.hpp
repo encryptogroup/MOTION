955a712418e79e158a14f27f3bd1692d2adb09f1
Lennart Braun <lenerd@posteo.de>
2020-04-13 05:24:27 +0200

    Add type_traits.hpp with is_unsigned_int_t trait
    We use want to allow unsigned integers uint8_t, uint16_t, etc. as
    template parameters quite often while excluding bool.  This is now
    caputed in the trait is_unsigned_int_t.

---
+++ src/motioncore/utility/type_traits.hpp
@@ -0,0 +1,34 @@
+// MIT License
+//
+// Copyright (c) 2020 Lennart Braun
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include <type_traits>
+
+namespace ENCRYPTO {
+
+template <typename T>
+using is_unsigned_int_t =
+    std::enable_if_t<std::conjunction_v<std::is_integral<T>, std::is_unsigned<T>,
+                                        std::negation<std::is_same<T, bool>>>>;
+
+}  // namespace ENCRYPTO
