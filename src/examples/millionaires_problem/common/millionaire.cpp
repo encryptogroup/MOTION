6b073fcbf5d786aac0fa4d3380164c6779bb379d
Tobias Kussel <kussel@cbs.tu-darmstadt.de>
2021-04-30 08:58:39 +0200

    fix spelling of millionaires´ problem

--- src/examples/millionaires_problem/common/millionaire.cpp
+++ src/examples/millionaires_problem/common/millionaire.cpp
@@ -22,7 +22,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "millionair.h"
+#include "millionaire.h"
 
 #include <cstddef>
 #include <vector>
