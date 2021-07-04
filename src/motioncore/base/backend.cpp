ce003ec867cdee11a85e1aabe8bd7dd97b424b93
arianneroselina <vincentiarianne@gmail.com>
2021-07-04 12:20:38 +0200

    move #include <boost/asio/thread_pool.hpp> to top to avoid error 'Winsock.h already included' in Windows

--- src/motioncore/base/backend.cpp
+++ src/motioncore/base/backend.cpp
@@ -22,6 +22,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <boost/asio/thread_pool.hpp>
+
 #include "backend.h"
 #include "motion_base_provider.h"
 
@@ -33,7 +35,6 @@
 #include <span>
 
 #include <fmt/format.h>
-#include <boost/asio/thread_pool.hpp>
 
 #include "communication/communication_layer.h"
 #include "communication/message.h"
