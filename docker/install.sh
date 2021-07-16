9daf5351c7a5c9c24c3472dc723d14e67e9a140c
Oleksandr Tkachenko <tkachenko@encrypto.cs.tu-darmstadt.de>
2021-07-16 10:22:51 +0200

    link against tcmalloc in Docker by default for better efficiency

--- docker/install.sh
+++ docker/install.sh
@@ -2,6 +2,5 @@
 git clone --recursive --config core.autocrlf=input https://github.com/encryptogroup/MOTION.git
 cd MOTION
 # build MOTION
-mkdir build && cd build
-cmake -DMOTION_BUILD_TESTS=On -DMOTION_BUILD_EXE=On -j 4 ..
-make -j 4
+cmake -DMOTION_BUILD_TESTS=On -DMOTION_BUILD_EXE=On -DMOTION_LINK_TCMALLOC=On -B build
+cmake --build build -- -j 4
