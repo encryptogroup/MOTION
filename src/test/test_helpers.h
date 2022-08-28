4fb6857e29d845ddc9bb5d5940cfbb0a72479d9c
liangzhao <liangzhao92@gmail.com>
2022-08-28 16:23:16 +0200

    Fix RandomRangeIntegerVector with std::uniform_int_distribution for generating random integers

--- src/test/test_helpers.h
+++ src/test/test_helpers.h
@@ -25,6 +25,7 @@
 #include <openssl/rand.h>
 #include <algorithm>
 #include <limits>
+#include <random>
 #include <vector>
 
 template <typename T>
@@ -58,27 +59,16 @@ inline std::vector<bool> RandomBoolVector(std::size_t size) {
   return bool_vector;
 }
 
-template <typename T>
-std::vector<T> RandomRangeIntegerVector(double min, double max, std::size_t n) {
-  std::vector<T> random_numbers;
+template <typename IntType = int>
+std::vector<IntType> RandomRangeIntegerVector(IntType min, IntType max, std::size_t n) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<IntType> distrib(min, max);
+
+  std::vector<IntType> random_numbers;
   random_numbers.reserve(n);
   for (std::size_t i = 0; i < n; i++) {
-    // rand() will produce a random integer between 0 ... RAND_MAX where RAND_MAX
-    // is a very large number... by dividing this number by RAND_MAX we will get
-    // a number in the range 0 ... 1.  We typecast rand() to a double to ensure
-    // that double division takes place as opposed to interger division which
-    // would result in 0 OR 1 exactly.
-    double random = ((double)rand()) / RAND_MAX;
-
-    // Take the number between 0-1 above and multiply it by (max - min) to get a
-    // number in the range of 0 ... (max - min)
-    double range = (max - min) * random;
-
-    // take this number in the range of 0 - (max-min) above and add min to it to
-    // get a number in the range of min ... max (adding min to 0 give us min,
-    // adding min to max-min gives us back max!)
-    T number = min + range;
-    random_numbers.emplace_back(number);
+    random_numbers.emplace_back(distrib(gen));
   }
 
   return random_numbers;
