385345499b3dd6e70e7c6804275a2fd47be8abba
liangzhao <liangzhao92@gmail.com>
2022-12-05 21:59:46 +0100

    test

--- src/test/test_liangzhao_integer_scaling_gaussian_mechanism.cpp
+++ src/test/test_liangzhao_integer_scaling_gaussian_mechanism.cpp
@@ -100,11 +100,407 @@ TEST(IntegerScalingGaussianMechanism,
               SecureIntegerScalingGaussianMechanism(share_fD);
           secure_integer_scaling_gaussian_mechanism.ParameterSetup(
               sensitivity, sigma, num_of_simd_gau, failure_probability_requirement);
-std::cout<<"001"<< std::endl;
 
           SecureFloatingPointCircuitABY floating_point_gaussian_noise =
               secure_integer_scaling_gaussian_mechanism.FLGaussianNoiseGeneration_naive();
-std::cout<<"002"<< std::endl;
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise_out =
+              floating_point_gaussian_noise.Out();
+
+          std::cout << "party run" << std::endl;
+          motion_parties.at(party_id)->Run();
+          motion_parties.at(party_id)->Finish();
+
+          std::cout << "party finish" << std::endl;
+          if (party_id == 0) {
+            std::cout << "party_id: " << party_id << std::endl;
+            std::vector<double> share_result_0_out_as =
+                floating_point_gaussian_noise_out.AsFloatingPointVector<double>();
+
+            for (std::size_t i = 0; i < share_result_0_out_as.size(); ++i) {
+              std::cout << "share_result_0_out_as[i]: " << double(share_result_0_out_as[i])
+                        << std::endl;
+            }
+          }
+        }
+      } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+      }
+    }
+  };
+  for (auto i = 0ull; i < kTestIterations; ++i) {
+    template_test(static_cast<std::uint64_t>(0));
+  }
+}
+
+TEST(IntegerScalingGaussianMechanism,
+     SecureIntegerScalingGaussianMechanism_32_64_GC_optimized_2_parties) {
+  constexpr auto kArithmeticGmw = encrypto::motion::MpcProtocol::kArithmeticGmw;
+  constexpr auto kArithmeticConstant = encrypto::motion::MpcProtocol::kArithmeticConstant;
+  constexpr auto kBooleanGmw = encrypto::motion::MpcProtocol::kBooleanGmw;
+  constexpr auto kBooleanConstant = encrypto::motion::MpcProtocol::kBooleanConstant;
+    constexpr auto kGarbledCircuit = encrypto::motion::MpcProtocol::kGarbledCircuit;
+  constexpr auto kBmr = encrypto::motion::MpcProtocol::kBmr;
+  auto template_test = [](auto template_variable_1) {
+    using T = decltype(template_variable_1);
+    using T_int = get_int_type_t<T>;
+    using A = std::allocator<T>;
+    std::srand(std::time(nullptr));
+
+    for (auto number_of_parties : kNumberOfPartiesList) {
+      std::size_t output_owner = 0;
+
+      std::size_t num_of_simd_gau = 1;
+      std::vector<double> fD_vector = rand_range_double_vector(0, 2, num_of_simd_gau);
+
+      long double failure_probability_requirement = std::exp(-20);
+
+      // only for debugging
+      // long double failure_probability_requirement = std::exp2l(-1);
+
+      double sensitivity = 1;
+      double sigma = 1;
+
+      try {
+        std::vector<PartyPointer> motion_parties(
+            std::move(MakeLocallyConnectedParties(number_of_parties, kPortOffset)));
+        for (auto& party : motion_parties) {
+          party->GetLogger()->SetEnabled(kDetailedLoggingEnabled);
+          party->GetConfiguration()->SetOnlineAfterSetup(std::mt19937{}() % 2 == 1);
+        }
+#pragma omp parallel num_threads(motion_parties.size() + 1) default(shared)
+#pragma omp single
+#pragma omp taskloop num_tasks(motion_parties.size())
+        for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+          encrypto::motion::ShareWrapper share_fD = motion_parties.at(party_id)->In<kGarbledCircuit>(
+              ToInput<double, std::true_type>(fD_vector), 0);
+
+          SecureIntegerScalingGaussianMechanism secure_integer_scaling_gaussian_mechanism =
+              SecureIntegerScalingGaussianMechanism(share_fD);
+          secure_integer_scaling_gaussian_mechanism.ParameterSetup(
+              sensitivity, sigma, num_of_simd_gau, failure_probability_requirement);
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise =
+              secure_integer_scaling_gaussian_mechanism.FLGaussianNoiseGeneration_naive();
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise_out =
+              floating_point_gaussian_noise.Out();
+
+          std::cout << "party run" << std::endl;
+          motion_parties.at(party_id)->Run();
+          motion_parties.at(party_id)->Finish();
+
+          std::cout << "party finish" << std::endl;
+          if (party_id == 0) {
+            std::cout << "party_id: " << party_id << std::endl;
+            std::vector<double> share_result_0_out_as =
+                floating_point_gaussian_noise_out.AsFloatingPointVector<double>();
+
+            for (std::size_t i = 0; i < share_result_0_out_as.size(); ++i) {
+              std::cout << "share_result_0_out_as[i]: " << double(share_result_0_out_as[i])
+                        << std::endl;
+            }
+          }
+        }
+      } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+      }
+    }
+  };
+  for (auto i = 0ull; i < kTestIterations; ++i) {
+    template_test(static_cast<std::uint64_t>(0));
+  }
+}
+
+
+
+TEST(IntegerScalingGaussianMechanism,
+     SecureIntegerScalingGaussianMechanism_32_64_BGMW_naive_2_parties) {
+  constexpr auto kArithmeticGmw = encrypto::motion::MpcProtocol::kArithmeticGmw;
+  constexpr auto kArithmeticConstant = encrypto::motion::MpcProtocol::kArithmeticConstant;
+  constexpr auto kBooleanGmw = encrypto::motion::MpcProtocol::kBooleanGmw;
+  constexpr auto kBooleanConstant = encrypto::motion::MpcProtocol::kBooleanConstant;
+    constexpr auto kGarbledCircuit = encrypto::motion::MpcProtocol::kGarbledCircuit;
+  constexpr auto kBmr = encrypto::motion::MpcProtocol::kBmr;
+  auto template_test = [](auto template_variable_1) {
+    using T = decltype(template_variable_1);
+    using T_int = get_int_type_t<T>;
+    using A = std::allocator<T>;
+    std::srand(std::time(nullptr));
+
+    for (auto number_of_parties : kNumberOfPartiesList) {
+      std::size_t output_owner = 0;
+
+      std::size_t num_of_simd_gau = 1;
+      std::vector<double> fD_vector = rand_range_double_vector(0, 2, num_of_simd_gau);
+
+      long double failure_probability_requirement = std::exp(-20);
+
+      // only for debugging
+      // long double failure_probability_requirement = std::exp2l(-1);
+
+      double sensitivity = 1;
+      double sigma = 1;
+
+      try {
+        std::vector<PartyPointer> motion_parties(
+            std::move(MakeLocallyConnectedParties(number_of_parties, kPortOffset)));
+        for (auto& party : motion_parties) {
+          party->GetLogger()->SetEnabled(kDetailedLoggingEnabled);
+          party->GetConfiguration()->SetOnlineAfterSetup(std::mt19937{}() % 2 == 1);
+        }
+#pragma omp parallel num_threads(motion_parties.size() + 1) default(shared)
+#pragma omp single
+#pragma omp taskloop num_tasks(motion_parties.size())
+        for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+          encrypto::motion::ShareWrapper share_fD = motion_parties.at(party_id)->In<kBooleanGmw>(
+              ToInput<double, std::true_type>(fD_vector), 0);
+
+          SecureIntegerScalingGaussianMechanism secure_integer_scaling_gaussian_mechanism =
+              SecureIntegerScalingGaussianMechanism(share_fD);
+          secure_integer_scaling_gaussian_mechanism.ParameterSetup(
+              sensitivity, sigma, num_of_simd_gau, failure_probability_requirement);
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise =
+              secure_integer_scaling_gaussian_mechanism.FLGaussianNoiseGeneration_naive();
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise_out =
+              floating_point_gaussian_noise.Out();
+
+          std::cout << "party run" << std::endl;
+          motion_parties.at(party_id)->Run();
+          motion_parties.at(party_id)->Finish();
+
+          std::cout << "party finish" << std::endl;
+          if (party_id == 0) {
+            std::cout << "party_id: " << party_id << std::endl;
+            std::vector<double> share_result_0_out_as =
+                floating_point_gaussian_noise_out.AsFloatingPointVector<double>();
+
+            for (std::size_t i = 0; i < share_result_0_out_as.size(); ++i) {
+              std::cout << "share_result_0_out_as[i]: " << double(share_result_0_out_as[i])
+                        << std::endl;
+            }
+          }
+        }
+      } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+      }
+    }
+  };
+  for (auto i = 0ull; i < kTestIterations; ++i) {
+    template_test(static_cast<std::uint64_t>(0));
+  }
+}
+
+TEST(IntegerScalingGaussianMechanism,
+     SecureIntegerScalingGaussianMechanism_32_64_BGMW_optimized_2_parties) {
+  constexpr auto kArithmeticGmw = encrypto::motion::MpcProtocol::kArithmeticGmw;
+  constexpr auto kArithmeticConstant = encrypto::motion::MpcProtocol::kArithmeticConstant;
+  constexpr auto kBooleanGmw = encrypto::motion::MpcProtocol::kBooleanGmw;
+  constexpr auto kBooleanConstant = encrypto::motion::MpcProtocol::kBooleanConstant;
+    constexpr auto kGarbledCircuit = encrypto::motion::MpcProtocol::kGarbledCircuit;
+  constexpr auto kBmr = encrypto::motion::MpcProtocol::kBmr;
+  auto template_test = [](auto template_variable_1) {
+    using T = decltype(template_variable_1);
+    using T_int = get_int_type_t<T>;
+    using A = std::allocator<T>;
+    std::srand(std::time(nullptr));
+
+    for (auto number_of_parties : kNumberOfPartiesList) {
+      std::size_t output_owner = 0;
+
+      std::size_t num_of_simd_gau = 1;
+      std::vector<double> fD_vector = rand_range_double_vector(0, 2, num_of_simd_gau);
+
+      long double failure_probability_requirement = std::exp(-20);
+
+      // only for debugging
+      // long double failure_probability_requirement = std::exp2l(-1);
+
+      double sensitivity = 1;
+      double sigma = 1;
+
+      try {
+        std::vector<PartyPointer> motion_parties(
+            std::move(MakeLocallyConnectedParties(number_of_parties, kPortOffset)));
+        for (auto& party : motion_parties) {
+          party->GetLogger()->SetEnabled(kDetailedLoggingEnabled);
+          party->GetConfiguration()->SetOnlineAfterSetup(std::mt19937{}() % 2 == 1);
+        }
+#pragma omp parallel num_threads(motion_parties.size() + 1) default(shared)
+#pragma omp single
+#pragma omp taskloop num_tasks(motion_parties.size())
+        for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+          encrypto::motion::ShareWrapper share_fD = motion_parties.at(party_id)->In<kBooleanGmw>(
+              ToInput<double, std::true_type>(fD_vector), 0);
+
+          SecureIntegerScalingGaussianMechanism secure_integer_scaling_gaussian_mechanism =
+              SecureIntegerScalingGaussianMechanism(share_fD);
+          secure_integer_scaling_gaussian_mechanism.ParameterSetup(
+              sensitivity, sigma, num_of_simd_gau, failure_probability_requirement);
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise =
+              secure_integer_scaling_gaussian_mechanism.FLGaussianNoiseGeneration_naive();
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise_out =
+              floating_point_gaussian_noise.Out();
+
+          std::cout << "party run" << std::endl;
+          motion_parties.at(party_id)->Run();
+          motion_parties.at(party_id)->Finish();
+
+          std::cout << "party finish" << std::endl;
+          if (party_id == 0) {
+            std::cout << "party_id: " << party_id << std::endl;
+            std::vector<double> share_result_0_out_as =
+                floating_point_gaussian_noise_out.AsFloatingPointVector<double>();
+
+            for (std::size_t i = 0; i < share_result_0_out_as.size(); ++i) {
+              std::cout << "share_result_0_out_as[i]: " << double(share_result_0_out_as[i])
+                        << std::endl;
+            }
+          }
+        }
+      } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+      }
+    }
+  };
+  for (auto i = 0ull; i < kTestIterations; ++i) {
+    template_test(static_cast<std::uint64_t>(0));
+  }
+}
+
+
+TEST(IntegerScalingGaussianMechanism,
+     SecureIntegerScalingGaussianMechanism_32_64_BMR_naive_2_parties) {
+  constexpr auto kArithmeticGmw = encrypto::motion::MpcProtocol::kArithmeticGmw;
+  constexpr auto kArithmeticConstant = encrypto::motion::MpcProtocol::kArithmeticConstant;
+  constexpr auto kBooleanGmw = encrypto::motion::MpcProtocol::kBooleanGmw;
+  constexpr auto kBooleanConstant = encrypto::motion::MpcProtocol::kBooleanConstant;
+    constexpr auto kGarbledCircuit = encrypto::motion::MpcProtocol::kGarbledCircuit;
+  constexpr auto kBmr = encrypto::motion::MpcProtocol::kBmr;
+  auto template_test = [](auto template_variable_1) {
+    using T = decltype(template_variable_1);
+    using T_int = get_int_type_t<T>;
+    using A = std::allocator<T>;
+    std::srand(std::time(nullptr));
+
+    for (auto number_of_parties : kNumberOfPartiesList) {
+      std::size_t output_owner = 0;
+
+      std::size_t num_of_simd_gau = 1;
+      std::vector<double> fD_vector = rand_range_double_vector(0, 2, num_of_simd_gau);
+
+      long double failure_probability_requirement = std::exp(-20);
+
+      // only for debugging
+      // long double failure_probability_requirement = std::exp2l(-1);
+
+      double sensitivity = 1;
+      double sigma = 1;
+
+      try {
+        std::vector<PartyPointer> motion_parties(
+            std::move(MakeLocallyConnectedParties(number_of_parties, kPortOffset)));
+        for (auto& party : motion_parties) {
+          party->GetLogger()->SetEnabled(kDetailedLoggingEnabled);
+          party->GetConfiguration()->SetOnlineAfterSetup(std::mt19937{}() % 2 == 1);
+        }
+#pragma omp parallel num_threads(motion_parties.size() + 1) default(shared)
+#pragma omp single
+#pragma omp taskloop num_tasks(motion_parties.size())
+        for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+          encrypto::motion::ShareWrapper share_fD = motion_parties.at(party_id)->In<kBmr>(
+              ToInput<double, std::true_type>(fD_vector), 0);
+
+          SecureIntegerScalingGaussianMechanism secure_integer_scaling_gaussian_mechanism =
+              SecureIntegerScalingGaussianMechanism(share_fD);
+          secure_integer_scaling_gaussian_mechanism.ParameterSetup(
+              sensitivity, sigma, num_of_simd_gau, failure_probability_requirement);
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise =
+              secure_integer_scaling_gaussian_mechanism.FLGaussianNoiseGeneration_naive();
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise_out =
+              floating_point_gaussian_noise.Out();
+
+          std::cout << "party run" << std::endl;
+          motion_parties.at(party_id)->Run();
+          motion_parties.at(party_id)->Finish();
+
+          std::cout << "party finish" << std::endl;
+          if (party_id == 0) {
+            std::cout << "party_id: " << party_id << std::endl;
+            std::vector<double> share_result_0_out_as =
+                floating_point_gaussian_noise_out.AsFloatingPointVector<double>();
+
+            for (std::size_t i = 0; i < share_result_0_out_as.size(); ++i) {
+              std::cout << "share_result_0_out_as[i]: " << double(share_result_0_out_as[i])
+                        << std::endl;
+            }
+          }
+        }
+      } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+      }
+    }
+  };
+  for (auto i = 0ull; i < kTestIterations; ++i) {
+    template_test(static_cast<std::uint64_t>(0));
+  }
+}
+
+TEST(IntegerScalingGaussianMechanism,
+     SecureIntegerScalingGaussianMechanism_32_64_BMR_optimized_2_parties) {
+  constexpr auto kArithmeticGmw = encrypto::motion::MpcProtocol::kArithmeticGmw;
+  constexpr auto kArithmeticConstant = encrypto::motion::MpcProtocol::kArithmeticConstant;
+  constexpr auto kBooleanGmw = encrypto::motion::MpcProtocol::kBooleanGmw;
+  constexpr auto kBooleanConstant = encrypto::motion::MpcProtocol::kBooleanConstant;
+    constexpr auto kGarbledCircuit = encrypto::motion::MpcProtocol::kGarbledCircuit;
+  constexpr auto kBmr = encrypto::motion::MpcProtocol::kBmr;
+  auto template_test = [](auto template_variable_1) {
+    using T = decltype(template_variable_1);
+    using T_int = get_int_type_t<T>;
+    using A = std::allocator<T>;
+    std::srand(std::time(nullptr));
+
+    for (auto number_of_parties : kNumberOfPartiesList) {
+      std::size_t output_owner = 0;
+
+      std::size_t num_of_simd_gau = 1;
+      std::vector<double> fD_vector = rand_range_double_vector(0, 2, num_of_simd_gau);
+
+      long double failure_probability_requirement = std::exp(-20);
+
+      // only for debugging
+      // long double failure_probability_requirement = std::exp2l(-1);
+
+      double sensitivity = 1;
+      double sigma = 1;
+
+      try {
+        std::vector<PartyPointer> motion_parties(
+            std::move(MakeLocallyConnectedParties(number_of_parties, kPortOffset)));
+        for (auto& party : motion_parties) {
+          party->GetLogger()->SetEnabled(kDetailedLoggingEnabled);
+          party->GetConfiguration()->SetOnlineAfterSetup(std::mt19937{}() % 2 == 1);
+        }
+#pragma omp parallel num_threads(motion_parties.size() + 1) default(shared)
+#pragma omp single
+#pragma omp taskloop num_tasks(motion_parties.size())
+        for (auto party_id = 0u; party_id < motion_parties.size(); ++party_id) {
+          encrypto::motion::ShareWrapper share_fD = motion_parties.at(party_id)->In<kBmr>(
+              ToInput<double, std::true_type>(fD_vector), 0);
+
+          SecureIntegerScalingGaussianMechanism secure_integer_scaling_gaussian_mechanism =
+              SecureIntegerScalingGaussianMechanism(share_fD);
+          secure_integer_scaling_gaussian_mechanism.ParameterSetup(
+              sensitivity, sigma, num_of_simd_gau, failure_probability_requirement);
+
+          SecureFloatingPointCircuitABY floating_point_gaussian_noise =
+              secure_integer_scaling_gaussian_mechanism.FLGaussianNoiseGeneration_naive();
 
           SecureFloatingPointCircuitABY floating_point_gaussian_noise_out =
               floating_point_gaussian_noise.Out();
