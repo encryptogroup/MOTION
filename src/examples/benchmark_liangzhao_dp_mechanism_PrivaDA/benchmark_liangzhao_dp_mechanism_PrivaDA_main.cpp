20d7769ad61bcda5e33f0c38cbb1a0dfefe6d7cd
liangzhao <liangzhao92@gmail.com>
2022-12-07 10:45:07 +0100

    test

--- src/examples/benchmark_liangzhao_dp_mechanism_PrivaDA/benchmark_liangzhao_dp_mechanism_PrivaDA_main.cpp
+++ src/examples/benchmark_liangzhao_dp_mechanism_PrivaDA/benchmark_liangzhao_dp_mechanism_PrivaDA_main.cpp
@@ -89,7 +89,7 @@ std::vector<Combination> GenerateAllCombinations() {
   };
 
   const std::array kProtocol = {
-      encrypto::motion::MpcProtocol::kGarbledCircuit,  // only for two parties
+      // encrypto::motion::MpcProtocol::kGarbledCircuit,  // only for two parties
       encrypto::motion::MpcProtocol::kBooleanGmw,
       // encrypto::motion::MpcProtocol::kBmr,  // too slow, 
   };
