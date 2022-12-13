47361576008ed02ed75fc88e8f8154908af61e94
liangzhao-darmstadt <liangzhao92@gmail.com>
2022-12-13 16:00:59 +0100

    test

--- src/examples/benchmark_liangzhao_signed_integer_operation/benchmark_liangzhao_signed_integer_operation_main.cpp
+++ src/examples/benchmark_liangzhao_signed_integer_operation/benchmark_liangzhao_signed_integer_operation_main.cpp
@@ -117,26 +117,9 @@ std::vector<Combination> GenerateAllCombinations() {
     }
   }
 
-  for (const auto number_of_simd : kNumbersOfSimd) {
-    for (const auto bit_size : kBooleanBitSizes) {
-      for (const auto operation_type : kGarbledCircuitOperationTypes) {
-        if ((bit_size == 8 || bit_size == 16) && (operation_type == T::kInt2FL)) {
-        }
-
-        else if ((bit_size == 8 || bit_size == 16) && (operation_type == T::kInt2Fx)) {
-        }
-
-        else {
-          combinations.emplace_back(bit_size, encrypto::motion::MpcProtocol::kGarbledCircuit,
-                                    operation_type, number_of_simd);
-        }
-      }
-    }
-  }
-
   // for (const auto number_of_simd : kNumbersOfSimd) {
   //   for (const auto bit_size : kBooleanBitSizes) {
-  //     for (const auto operation_type : kBmrOperationTypes) {
+  //     for (const auto operation_type : kGarbledCircuitOperationTypes) {
   //       if ((bit_size == 8 || bit_size == 16) && (operation_type == T::kInt2FL)) {
   //       }
 
@@ -144,13 +127,30 @@ std::vector<Combination> GenerateAllCombinations() {
   //       }
 
   //       else {
-  //         combinations.emplace_back(bit_size, encrypto::motion::MpcProtocol::kBmr,
-  //         operation_type,
-  //                                   number_of_simd);
+  //         combinations.emplace_back(bit_size, encrypto::motion::MpcProtocol::kGarbledCircuit,
+  //                                   operation_type, number_of_simd);
   //       }
   //     }
   //   }
   // }
+
+  for (const auto number_of_simd : kNumbersOfSimd) {
+    for (const auto bit_size : kBooleanBitSizes) {
+      for (const auto operation_type : kBmrOperationTypes) {
+        if ((bit_size == 8 || bit_size == 16) && (operation_type == T::kInt2FL)) {
+        }
+
+        else if ((bit_size == 8 || bit_size == 16) && (operation_type == T::kInt2Fx)) {
+        }
+
+        else {
+          combinations.emplace_back(bit_size, encrypto::motion::MpcProtocol::kBmr,
+          operation_type,
+                                    number_of_simd);
+        }
+      }
+    }
+  }
 
   // combinations.emplace_back(32, encrypto::motion::MpcProtocol::kBooleanGmw, T::kInt2FL, 1);
   // combinations.emplace_back(64, encrypto::motion::MpcProtocol::kBooleanGmw, T::kInt2FL, 1);
